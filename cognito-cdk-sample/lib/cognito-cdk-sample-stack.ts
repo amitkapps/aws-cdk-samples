627ca5a05c058c183a9afc2272bb2fd8e2d3d00c
Amit Kapoor <amitk.techno@gmail.com>
2024-03-21 19:01:45 -0700

    Added scopes for the resource server

--- cognito-cdk-sample/lib/cognito-cdk-sample-stack.ts
+++ cognito-cdk-sample/lib/cognito-cdk-sample-stack.ts
@@ -9,13 +9,32 @@ export class CognitoCdkSampleStack extends cdk.Stack {
         super(scope, id, props);
 
         const userPool = new cognito.UserPool(this, 'userPool', {
-            userPoolName: 'catalog-service-user-pool'
+            userPoolName: 'product-service-user-pool'
         });
 
-        // 👇 create an Output
-        new cdk.CfnOutput(this, 'GreetingsQueueArn', {
+        //Add a domain prefix for the url that is generated for the pool
+        new cognito.UserPoolDomain(this, 'userPoolDomain', {
+            userPool: userPool,
+            cognitoDomain: {domainPrefix: 'product-service'}
+        });
+
+        let addProductScope = new cognito.ResourceServerScope({scopeName: "catalog-service:add-product", scopeDescription: "Add new products to the catalog"});
+        let listProductScope = new cognito.ResourceServerScope({scopeName: "catalog-service:list-product", scopeDescription: "Allows listing all products from the catalog"});
+
+        userPool.addResourceServer('CatalogResourceServer', {
+            identifier: 'catalog-resource-server',
+            scopes: [addProductScope, listProductScope]
+        });
+
+
+        // Create an Output
+        new cdk.CfnOutput(this, 'UserPoolUrl', {
             value: userPool.userPoolProviderUrl,
             description: 'The Url of the user pool'
+        });
+        new cdk.CfnOutput(this, 'UserPoolId', {
+            value: userPool.userPoolId,
+            description: 'The User pool id'
         });
 
     }
